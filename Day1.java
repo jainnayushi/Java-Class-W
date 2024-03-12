58cd0120e8e25248bc537b315efbb10da7b7286c
Ayushi Jain <64698800+jainnayushi@users.noreply.github.com>
2024-03-12 13:26:49 +0530

    Notes

---
+++ Day1.java
@@ -0,0 +1,13 @@
+public class Day1 {
+    public static void main(String[] args) {
+        System.err.println("Started");
+        // Algorithm - PseudoCode
+//        step 1 : take utensil
+//        html css :
+        String name;
+//        name String;
+
+
+
+    }
+}
---
+++ Day2.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class Day2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//       Hello : ln
+//        World
+//
+//        Hello :\n
+//        World
+//
+//        Hello World or HelloWorld
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println();
+        System.out.println("Hello\nWorld");
+        System.out.println();
+        System.out.print("Hello");
+        System.out.println("World");
+
+//        System.out.print("What is your name : ");
+//        What is your name : hkhskh
+//        What is your name :
+//        djvdh
+
+
+//        *
+//        * *
+//        * * *
+//        * * * *
+
+//        System.out.printf(""); %d %s %f
+
+        System.out.println(1);
+        System.out.println("1");
+
+        System.out.println();
+
+        // + Operator
+        System.out.println(1+1); // 2 : Addition : num + num
+        System.out.println(1+"1");  //11 : Concatenation : num + string or string + string
+        System.out.println("1" + "1"); // 11
+        System.out.println("Hello" + 123); // Hello123
+        System.out.println(123+"123"); // 123123
+        System.out.println(123 + " 123"); // 123 123
+        System.out.println(123 +"-"+ "123"); // 123-123
+
+        System.out.println();
+
+
+//        System.out.print("What is your name : ");
+//        String name = sc.nextLine();
+//        System.out.print("What is your age : ");
+//        int age = sc.nextInt();
+//        System.out.println("Hello "+name+"! Cheers to "+age+" years"); // Hello ayushi Cheers to 18 years
+//        System.out.println("In next 10 years, you will be " + (age+10)); // 1810
+
+//        System.out.print("What is your age : ");
+//        String age = sc.nextLine();
+//        System.out.println(age+10); // 1810
+
+
+        int age = 10;
+        long population = 10000000l;
+    }
+}
+
